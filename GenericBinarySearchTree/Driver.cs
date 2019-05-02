8dcc9497f8828fd3f1513e5f5fbb02cc0ca42ce8
seafea <seafea@vt.edu>
2019-05-02 11:34:47 -0400

    Testing GetHeight in Driver
    New testing

--- GenericBinarySearchTree/Driver.cs
+++ GenericBinarySearchTree/Driver.cs
@@ -22,6 +22,8 @@ namespace GenericBinarySearchTree
             testBST.Insert(elementToInsert: 70);
             testBST.Insert(elementToInsert: 80);
             Console.WriteLine(testBST.ToString());
+            Queue<BinaryNode<int>> queue = testBST.GetLevelOrderTraversal();
+            Console.WriteLine("Height is " + testBST.GetHeight());
             Console.ReadLine();
         }
     }
